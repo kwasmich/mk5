6f9f2fa512da56e9d935d605d3c031fe1e16e1c1
Michael Kwasnicki <git@kwasi-ich.de>
2020-06-11 21:37:24 +0200

    * improve template

--- elements/_template.js
+++ elements/_template.js
@@ -1,11 +1,26 @@
-class MyElement extends HTMLElement {
+import HTMLCustomElement from "../html_custom_element.js";
+import Observable from "../util/observable.js";
+
+
+
+const template = new Observable();
+const priv = Symbol("private");
+
+
+
+class MyElement extends HTMLCustomElement {
     static get observedAttributes() {
         return [];
     }
 
 
     constructor() {
-        super();
+        super(template, "elements/_template");
+        this[priv] = this[priv] || {};
+        this[priv].shadowRoot = this.attachShadow({mode: 'closed'});
+        Object.seal(this);
+
+        template.subscribe((value) => value && this._init(value));
     }
 
 
@@ -13,6 +28,12 @@ class MyElement extends HTMLElement {
     attributeChangedCallback(name, oldValue, newValue) {}
     connectedCallback() {}
     disconnectedCallback() {}
+
+
+    _init() {
+        const content = template.value.content.cloneNode(true);
+        this[priv].shadowRoot.appendChild(content);
+    }
 }
 
 
