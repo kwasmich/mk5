ac20e0dbe4fe0a22d17d64f8f0198eb893b8db02
Michael Kwasnicki <git@kwasi-ich.de>
2020-11-01 14:38:31 +0100

    * rearranging colors * using fetch instead XHR

--- util/helper.js
+++ util/helper.js
@@ -1,4 +1,5 @@
 export function loadText(PATH) {
+    return fetch(PATH).then(response => response.text());
     const promise = (resolve, reject) => {
         let loadHandler, xhr;
         
@@ -24,6 +25,7 @@ export function loadText(PATH) {
 
 
 export function loadHTML(PATH) {
+    return fetch(PATH).then(response => response.text());
     const promise = (resolve, reject) => {
         let loadHandler, xhr;
         
@@ -55,7 +57,40 @@ export function loadHTML(PATH) {
 
 
 
+// export function loadHTML(PATH) {
+//     const promise = (resolve, reject) => {
+//         let loadHandler, xhr;
+        
+//         loadHandler = (EVENT) => {
+//             if (EVENT.currentTarget.status === 200) {
+//                 const response = EVENT.currentTarget.responseText;
+//                 resolve(response);
+//             } else {
+//                 reject(EVENT.currentTarget);
+//             }
+//         };
+
+//         xhr = new XMLHttpRequest();
+//         xhr.onload = loadHandler;
+//         xhr.onerror = reject;
+//         xhr.open("GET", PATH, true);
+//         // xhr.setRequestHeader("Cache-Control", "no-cache"); 
+//         // xhr.setRequestHeader("Pragma", "no-cache"); 
+//         // xhr.setRequestHeader('cache-control', 'no-cache, must-revalidate, post-check=0, pre-check=0');
+//         // xhr.setRequestHeader('cache-control', 'max-age=0');
+//         // xhr.setRequestHeader('expires', '0');
+//         // xhr.setRequestHeader('expires', 'Tue, 01 Jan 1980 1:00:00 GMT');
+//         // xhr.setRequestHeader('pragma', 'no-cache');
+//         xhr.send();
+//     };
+    
+//     return new Promise(promise);
+// }
+
+
+
 export function loadJSON(URL) {
+    return fetch(PATH).then(response => response.json());
     const promise = (resolve, reject) => {
         let loadHandler, xhr;
 
