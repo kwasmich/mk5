a769b511d0da0e525e68d1298be42ab8836567b0
Michael Kwasnicki <git@kwasi-ich.de>
2020-11-08 16:42:52 +0100

    * stack and dual column navigation view

--- base/ui-navigation-view/ui-navigation-view.js
+++ base/ui-navigation-view/ui-navigation-view.js
@@ -3,7 +3,15 @@ import UIView from "/base/ui-view.js";
 
 
 const priv = Symbol("private");
+const mediaQuery = window.matchMedia("screen and (max-width : 640px)");
+const DEFAULT_NAVIGATION_STYLE = "default";
+const STACK_NAVIGATION_STYLE = "stack";
+const DOUBLE_COLUMN_NAVIGATION_STYLE = "double";
 
+const SLOT = {
+    "stack": "primary",
+    "double": "secondary"
+}
 
 
 export default class UINavigationView extends UIView {
@@ -12,8 +20,13 @@ export default class UINavigationView extends UIView {
     }
 
 
+    get _currentSlotName() {
+        return SLOT[this[priv].navigationViewStyle];
+    }
+
+
     get _currentView() {
-        const slot = this[priv].shadowRoot.querySelector("slot[name=secondary]");
+        const slot = this[priv].shadowRoot.querySelector(`slot[name=${this._currentSlotName}]`);
         const currentView = slot.assignedElements()[0];
         return currentView;
     }
@@ -23,7 +36,11 @@ export default class UINavigationView extends UIView {
         const self = super(args);
 
         this[priv] = this[priv] ?? {};
+        this[priv].navigationViewStyle = DOUBLE_COLUMN_NAVIGATION_STYLE;
         this[priv].shadowRoot = this.attachShadow({ mode: "closed" });
+
+        this._onMediaQuery = this._onMediaQuery.bind(this);
+
         Object.seal(this);
         Object.seal(this[priv]);
 
@@ -34,14 +51,28 @@ export default class UINavigationView extends UIView {
 
     adoptedCallback() {}
     attributeChangedCallback(name, oldValue, newValue) {}
-    connectedCallback() {}
-    disconnectedCallback() {}
+    
+
+    connectedCallback() {
+        mediaQuery.addListener(this._onMediaQuery);
+        this._onMediaQuery(mediaQuery);
+    }
+    
+    
+    disconnectedCallback() {
+        mediaQuery.removeListener(this._onMediaQuery);
+    }
+
+
+    onInit() {
+        console.log("onInit");
+    }
 
 
     pushView(view) {
         this.appendChild(view);
-        view.setAttribute("slot", "secondary");
         view.previousElementSibling.removeAttribute("slot");
+        view.setAttribute("slot", this._currentSlotName);
     }
 
 
@@ -50,7 +81,7 @@ export default class UINavigationView extends UIView {
         const previousView = currentView.previousElementSibling;
 
         if (previousView && !previousView.hasAttribute("slot")) {
-            previousView.setAttribute("slot", "secondary");
+            previousView.setAttribute("slot", this._currentSlotName);
             this.removeChild(currentView);
             return previousView;
         } else {
@@ -64,7 +95,7 @@ export default class UINavigationView extends UIView {
         let currentView = this._currentView;
 
         if ([...this.children].includes(view)) {
-            view.setAttribute("slot", "secondary");
+            view.setAttribute("slot", this._currentSlotName);
 
             while (view.nextElementSibling) {
                 this.removeChild(view.nextElementSibling);
@@ -85,13 +116,43 @@ export default class UINavigationView extends UIView {
             currentView = currentView.previousElementSibling;
         }
 
-        currentView.setAttribute("slot", "secondary");
+        currentView.setAttribute("slot", this._currentSlotName);
 
         while (currentView.nextElementSibling) {
             this.removeChild(currentView.nextElementSibling);
         }
 
         return currentView;
+    }
+
+
+    _onMediaQuery(mediaQueryListEvent) {
+        this[priv].navigationViewStyle = mediaQueryListEvent.matches ? STACK_NAVIGATION_STYLE : DOUBLE_COLUMN_NAVIGATION_STYLE;
+        console.log(this[priv].navigationViewStyle);
+        const primary = this.querySelector("[slot=primary]");
+        const secondary = this.querySelector("[slot=secondary]");
+
+        if (this[priv].navigationViewStyle === STACK_NAVIGATION_STYLE) {
+            if (secondary.previousElementSibling === primary) {
+                this.insertBefore(secondary, this.firstElementChild);
+            } else if (primary.nextElementSibling) {
+                primary.nextElementSibling.setAttribute("slot", "secondary");
+                this.insertBefore(primary.nextElementSibling, this.firstElementChild);
+                primary.removeAttribute("slot");
+                secondary.setAttribute("slot", "primary");
+            }
+        }
+
+        if (this[priv].navigationViewStyle === DOUBLE_COLUMN_NAVIGATION_STYLE) {
+            if (primary.previousElementSibling === secondary) {
+                this.appendChild(secondary);
+            } else if (secondary.nextElementSibling) {
+                secondary.nextElementSibling.setAttribute("slot", "primary");
+                this.insertBefore(secondary.nextElementSibling, secondary);
+                secondary.removeAttribute("slot");
+                primary.setAttribute("slot", "secondary");
+            }
+        }
     }
 }
 
--- elements/navigation/dummy_b/dummy_b.js
+++ elements/navigation/dummy_b/dummy_b.js
@@ -1,5 +1,6 @@
 import UIView from "/base/ui-view.js";
-
+import DummyA from "../dummy_a/dummy_a.js";
+import DummyC from "../dummy_c/dummy_c.js";
 
 
 const priv = Symbol("private");
@@ -16,7 +17,12 @@ export default class DummyB extends UIView {
         const self = super(args);
 
         this[priv] = this[priv] ?? {};
+        this[priv].isConnected = false;
+        this[priv].isInited = false;
         this[priv].shadowRoot = this.attachShadow({ mode: "closed" });
+        
+        this._onClick = this._onClick.bind(this);
+
         Object.seal(this);
         Object.seal(this[priv]);
 
@@ -25,10 +31,61 @@ export default class DummyB extends UIView {
     }
 
 
+    onInit() {
+        super.onInit();
+        this[priv].isInited = true;
+        this._connect();
+    }
+
+
     adoptedCallback() {}
     attributeChangedCallback(name, oldValue, newValue) {}
-    connectedCallback() {}
-    disconnectedCallback() {}
+
+
+    connectedCallback() {
+        this[priv].isConnected = true;
+        this._connect();
+    }
+
+
+    disconnectedCallback() {
+        this[priv].shadowRoot.getElementById("a").removeEventListener("click", this._onClick);
+        this[priv].shadowRoot.getElementById("b").removeEventListener("click", this._onClick);
+        this[priv].shadowRoot.getElementById("c").removeEventListener("click", this._onClick);
+        this[priv].shadowRoot.getElementById("pop").removeEventListener("click", this._onClick);
+        this[priv].isConnected = false;
+    }
+
+
+    _connect() {
+        if (this[priv].isConnected && this[priv].isInited) {
+            this[priv].shadowRoot.getElementById("a").addEventListener("click", this._onClick);
+            this[priv].shadowRoot.getElementById("b").addEventListener("click", this._onClick);
+            this[priv].shadowRoot.getElementById("c").addEventListener("click", this._onClick);
+            this[priv].shadowRoot.getElementById("pop").addEventListener("click", this._onClick);
+        }
+    }
+
+
+    _onClick(mouseEvent) {
+        switch (mouseEvent.currentTarget.textContent) {
+            case "A":
+                this.parentNode.pushView(new DummyA());
+                break;
+
+            case "B":
+                this.parentNode.pushView(new DummyB());
+                break;
+
+            case "C":
+                this.parentNode.pushView(new DummyC());
+                break;
+
+            default:
+                this.parentNode.popView();
+                break;
+        }
+    }
 }
 
 
