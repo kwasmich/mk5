8daa816ad90f887cfb703db4ac86d54352eb8001
Michael Kwasnicki <git@kwasi-ich.de>
2021-11-20 11:43:23 +0100

    + light list + light list item

---
+++ hue/elements/hue-light-list-item/hue-light-list-item.js
@@ -0,0 +1,130 @@
+import { UIView } from "/base/ui-view.js";
+import { ct2rgb, IconMap, xy2rgb } from "/hue/hue-utils.js";
+
+
+
+
+export class HueLightListItem extends UIView {
+    static get observedAttributes() {
+        return [];
+    }
+
+
+    #shadowRoot = this.attachShadow({ mode: "closed" });
+    #initialized = false;
+    #light = undefined;
+    #icon = undefined;
+    #name = undefined;
+    #on = undefined;
+    #bri = undefined;
+
+
+    get item() {
+        return this.#light
+    }
+    
+
+    set item(val) {
+        this.#light = val ?? {};
+        this._updateView();
+    }
+
+
+    constructor(...args) {
+        const self = super(args);
+        Object.seal(this);
+
+        this._init(this.#shadowRoot);
+        this.onInit();
+        return self;
+    }
+
+
+    adoptedCallback() {}
+    attributeChangedCallback(name, oldValue, newValue) {}
+    connectedCallback() {}
+    disconnectedCallback() {}
+
+
+    onInit() {
+        // this.onclick = (mouseEvent) => this._onClick(mouseEvent);            // FIX ME: this is conflicting with UIListView
+        // this.addEventListener("click", (mouseEvent) => this._onClick(mouseEvent));
+
+        const sr = this.#shadowRoot;
+        this.#icon = sr.querySelector("img");
+        this.#name = sr.querySelector("span");
+        this.#on = sr.querySelector("input[type=checkbox]");
+        this.#on.onchange = (e) => this._onInputChange(e);
+        this.#bri = sr.querySelector("input[type=range]");
+        this.#bri.onchange = (e) => this._onInputChange(e);
+        this.#initialized = true;
+        this._updateView();
+    }
+
+
+    _updateView() {
+        if (this.#initialized && this.#light) {
+            const { on, bri, ct, hue, sat, xy, colormode, reachable } = this.#light.state
+            
+            if (this.#icon.src !== window.origin + IconMap[this.#light.class]) {
+                this.#icon.src = IconMap[this.#light.class];
+            }
+
+            this.#name.textContent = this.#light.name;
+            this.#on.checked = on;
+            this.#bri.value = bri;
+
+            if (on) {
+                switch (colormode) {
+                    case "xy":
+                        {
+                            const [x, y] = xy;
+                            const color = xy2rgb(x, y, 64 + bri / 255 * 191);
+                            // console.log(color);
+                            this.style.backgroundColor = `rgb(${color.r}, ${color.g}, ${color.b})`;
+                        }
+                        break;
+
+                    case "ct":
+                        {
+                            const color = ct2rgb(ct);
+                            // console.log(color);
+                            this.style.backgroundColor = `rgb(${color.r}, ${color.g}, ${color.b})`;
+                        }
+                        break;
+
+                    case "hs":
+                        {
+                            this.style.backgroundColor = "lime";
+                        }
+                        break;
+                }
+            } else {
+                this.style.backgroundColor = "#444";
+            }
+
+            this.classList.toggle("unreachable", !reachable);
+        }
+    }
+
+
+    _onInputChange(event) {
+        event.preventDefault();
+        event.stopImmediatePropagation();
+        const attribute = event.target.name;
+        const light = this.#light;
+        
+        switch (event.target.type) {
+            case "checkbox":
+                light[attribute] = event.target.checked;
+            break;
+            
+            default:
+                light[attribute] = +event.target.value;
+        }
+    }
+}
+
+
+
+UIView.define("hue-light-list-item", HueLightListItem, import.meta.url);
---
+++ hue/elements/hue-light-list/hue-light-list.js
@@ -0,0 +1,68 @@
+import { UIView } from "/base/ui-view.js";
+import Hue from "/hue/hue.js";
+
+
+
+export class HueLightList extends UIView {
+    static get observedAttributes() {
+        return [];
+    }
+
+
+    #shadowRoot = this.attachShadow({ mode: "closed" });
+    #hueGroup = undefined;
+    #lights = [];
+    #lightsSubscription = undefined;
+
+
+    get hueGroup() {
+        return this.#hueGroup;
+    }
+
+
+    set hueGroup(val) {
+        this.#hueGroup = val;
+        this._updateList();
+    }
+
+
+    constructor(hueGroup, ...args) {
+        const self = super(args);
+        Object.seal(this);
+
+        this.#hueGroup = hueGroup;
+
+        this._init(this.#shadowRoot);
+        this.onInit();
+        return self;
+    }
+
+
+    adoptedCallback() {}
+    attributeChangedCallback(name, oldValue, newValue) {}
+    connectedCallback() {}
+    disconnectedCallback() {}
+    
+
+    onInit() {
+        this.#lightsSubscription = (value) => this._updateLights(value);
+        Hue.lights.subscribe(this.#lightsSubscription);
+    }
+
+
+    _updateLights(lightsObj) {
+        this.#lights = Object.values(lightsObj ?? {});
+        this._updateList();
+    }
+
+
+    _updateList() {
+        const lights = this.#lights.filter((l) => this.#hueGroup?.lights.includes(l.id));
+        const listView = this.#shadowRoot.querySelector("ui-list-view");
+        listView.listData = lights;
+    }
+}
+
+
+
+UIView.define("hue-light-list", HueLightList, import.meta.url);
