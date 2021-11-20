4ef491b0bd89595430911ffc52b74ec5360b4fb4
Michael Kwasnicki <git@kwasi-ich.de>
2021-11-20 11:45:17 +0100

    * tabbed detail view

--- hue/elements/hue-room-detail/hue-room-detail.js
+++ hue/elements/hue-room-detail/hue-room-detail.js
@@ -1,5 +1,9 @@
+import "../hue-light-list-item/hue-light-list-item.js";
+import "../hue-light-list/hue-light-list.js";
+import "../hue-scene-list-item/hue-scene-list-item.js";
+import "../hue-scene-list/hue-scene-list.js";
 import { UIView } from "/base/ui-view.js";
-
+import Hue from "/hue/hue.js";
 
 
 export class HueRoomDetail extends UIView {
@@ -9,8 +13,9 @@ export class HueRoomDetail extends UIView {
 
 
     #shadowRoot = this.attachShadow({ mode: "closed" });
+    #groupsSubscription = undefined;
     #hueGroup = undefined;
-
+    #hueGroups = [];
 
     get hueGroup() {
         return this.#hueGroup;
@@ -19,6 +24,8 @@ export class HueRoomDetail extends UIView {
 
     set hueGroup(val) {
         this.#hueGroup = val;
+        this.#hueGroups = [val];
+        this._updateRoom();
         this._updateList();
     }
 
@@ -42,12 +49,31 @@ export class HueRoomDetail extends UIView {
     onInit() {
         const backButton = this.#shadowRoot.querySelector("button");
         backButton.onclick = () => this.parentNode.popToRootView();
+
+        this.#groupsSubscription  = (value) => {
+            this.#hueGroups = Object.values(value ?? {});
+            this._updateRoom();
+        };
+        Hue.groups.subscribe(this.#groupsSubscription);
+
+        this._updateRoom();
         this._updateList();
     }
 
+
+    _updateRoom() {
+        const hueGroup = this.#hueGroups?.find((g) => g.groupID === this.#hueGroup?.groupID);
+        const roomListItem = this.#shadowRoot.querySelector("hue-room-list-item");
+        roomListItem.item = hueGroup;
+    }
+
+
     _updateList() {
-        const sceneList = this.#shadowRoot.querySelector("hue-scene-list");
+        const tabView = this.#shadowRoot.querySelector("ui-tab-view");
+        const sceneList = tabView.views.find((x) => x.tagName.toLowerCase() === "hue-scene-list")
         sceneList.hueGroup = this.#hueGroup;
+        const lightList = tabView.views.find((x) => x.tagName.toLowerCase() === "hue-light-list");
+        lightList.hueGroup = this.#hueGroup;
     }
 }
 
