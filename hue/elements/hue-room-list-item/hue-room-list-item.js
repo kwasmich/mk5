1273c7a0667035315496cdfa4020e37a6eddc70b
Michael Kwasnicki <git@kwasi-ich.de>
2021-11-23 18:58:30 +0100

    * reflect room lights color and brightness on room item

--- hue/elements/hue-room-list-item/hue-room-list-item.js
+++ hue/elements/hue-room-list-item/hue-room-list-item.js
@@ -1,5 +1,41 @@
 import { UIView } from "/base/ui-view.js";
 import { ct2rgb, IconMap, xy2rgb } from "/hue/hue-utils.js";
+import Hue from "/hue/hue.js";
+
+
+function color(state) {
+    const { on, bri, ct, hue, sat, xy, colormode, reachable } = state;
+
+    if (on && reachable) {
+        switch (colormode) {
+            case "xy":
+                {
+                    const [x, y] = xy;
+                    const color = xy2rgb(x, y, 64 + bri / 255 * 191);
+                    return `rgb(${color.r}, ${color.g}, ${color.b})`;
+                }
+
+            case "ct":
+                {
+                    const color = ct2rgb(ct); 
+                    return `rgb(${color.r}, ${color.g}, ${color.b})`;
+                }
+
+            case "hs":
+                {
+                    return "lime";
+                }
+
+            default:
+                {
+                    const color = 64 + bri / 255 * 191;
+                    return `rgb(${color}, ${color}, ${color})`;
+                }
+        }
+    } else {
+        return "";
+    }
+}
 
 
 
@@ -16,6 +52,8 @@ export class HueRoomListItem extends UIView {
     #name = undefined;
     #on = undefined;
     #bri = undefined;
+    #lightsSubscription = undefined;
+    #lights = {};
 
 
     get item() {
@@ -48,6 +86,9 @@ export class HueRoomListItem extends UIView {
     onInit() {
         // this.onclick = (mouseEvent) => this._onClick(mouseEvent);
 
+        this.#lightsSubscription = (value) => this._updateLights(value);
+        Hue.lights.subscribe(this.#lightsSubscription);
+
         const sr = this.#shadowRoot;
         this.#icon = sr.querySelector("img");
         this.#name = sr.querySelector("span");
@@ -56,6 +97,12 @@ export class HueRoomListItem extends UIView {
         this.#bri = sr.querySelector("input[type=range]");
         this.#bri.onchange = (e) => this._onInputChange(e);
         this.#initialized = true;
+        this._updateView();
+    }
+
+
+    _updateLights(lightsObj) {
+        this.#lights = lightsObj;
         this._updateView();
     }
 
@@ -72,32 +119,52 @@ export class HueRoomListItem extends UIView {
             this.#on.checked = this.#room.state.any_on; // on
             this.#bri.value = bri;
 
-            if (this.#room.state.any_on) {
-                switch (colormode) {
-                    case "xy":
-                        {
-                            const [x, y] = xy;
-                            const color = xy2rgb(x, y, 64 + bri / 255 * 191);
-                            // console.log(color);
-                            this.style.backgroundColor = `rgb(${color.r}, ${color.g}, ${color.b})`;
-                        }
-                        break;
+            // if (on) { //(this.#room.state.any_on) {
+            //     switch (colormode) {
+            //         case "xy":
+            //             {
+            //                 const [x, y] = xy;
+            //                 const color = xy2rgb(x, y, 64 + bri / 255 * 191);
+            //                 // console.log(color);
+            //                 this.style.backgroundColor = `rgb(${color.r}, ${color.g}, ${color.b})`;
+            //             }
+            //             break;
 
-                    case "ct":
-                        {
-                            const color = ct2rgb(ct);
-                            // console.log(color);
-                            this.style.backgroundColor = `rgb(${color.r}, ${color.g}, ${color.b})`;
-                        }
-                        break;
+            //         case "ct":
+            //             {
+            //                 const color = ct2rgb(ct);
+            //                 // console.log(color);
+            //                 this.style.backgroundColor = `rgb(${color.r}, ${color.g}, ${color.b})`;
+            //             }
+            //             break;
 
-                    case "hs":
-                        {
-                            this.style.backgroundColor = "lime";
-                        }
-                        break;
-                }
+            //         case "hs":
+            //             {
+            //                 this.style.backgroundColor = "lime";
+            //             }
+            //             break;
+            //     }
+            // } else {
+            //     this.style.backgroundColor = "#444";
+            // }
+
+            // console.log(this.#room);
+            // console.log(this.#lights);
+
+            const lights = this.#room.lights.sort((a,b) => +a - +b).map((l) => this.#lights[l]);
+            const colors = lights.map((l) => color(l.state)).filter((s) => !!s);
+
+            while (colors.length < lights.length) {
+                colors.push("#444");
+            }
+
+            if (colors.length === 1) {
+                this.style.background = undefined;
+                this.style.backgroundColor = colors.join();
+            } else if (colors.length > 1) {
+                this.style.background = `linear-gradient(90deg, ${colors.join()})`;
             } else {
+                this.style.background = undefined;
                 this.style.backgroundColor = "#444";
             }
         }
