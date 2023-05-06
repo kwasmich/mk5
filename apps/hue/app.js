7b031128266c5a4c0f0d2f3a19994b1688b3d058
Michael Stocker <michael.stocker@kwasi-ich.de>
2023-05-06 12:20:46 +0200

    * don't load all the apps already in launcher

---
+++ apps/hue/app.js
@@ -0,0 +1,55 @@
+import { UIView } from "/base/ui-view.js";
+
+
+export class AppHue extends UIView {
+    static get observedAttributes() {
+        return [];
+    }
+
+
+    #shadowRoot = this.attachShadow({ mode: "closed" });
+    #app = undefined;
+    
+
+    constructor(...args) {
+        const self = super(args);
+        Object.seal(this);
+
+        this._init(this.#shadowRoot);
+        return self;
+    }
+
+
+    adoptedCallback() {}
+    attributeChangedCallback(name, oldValue, newValue) {}
+
+    connectedCallback() {
+        console.log("connected");
+
+        if (!this.#app) {
+            this.#app = this._loadApp();
+        } 
+        
+        this.#app.then((app) => {
+            this.parentElement.replaceChild(app, this);
+            app.id = this.id;
+        });
+    }
+
+    disconnectedCallback() {}
+
+
+    async _loadApp() {
+        const { MyHue } = await import("/elements/my_hue/my_hue.js");
+        console.log("loaded");
+        const name = UIView.registry.get(MyHue);
+        await(customElements.whenDefined(name));
+        console.log("registred");
+        const app = new MyHue();
+        return app;
+    }
+}
+
+
+
+UIView.define("app-hue", AppHue, import.meta.url);
